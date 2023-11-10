d95ae2f5cb0fdfd2ae6b97f55720b550e8ccb441
Ryan Bliss <smile@ryanbliss.me>
2023-11-10 01:41:52 -0800

    fixed join POST requests

--- src/api/postRecentViewedProject.ts
+++ src/api/postRecentViewedProject.ts
@@ -1,13 +1,10 @@
 "use server";
 
-"use server"
-
 import { MongoClient } from "mongodb";
 import { getUserId } from ".";
 import { CodeboxMongoClient } from "./internal";
 import { IJoin, IPostRecentViewedProjectBody, JoinType } from "@/models";
-import { JOIN_COLLECTION_ID, PROJECTS_COLLECTION_ID } from "@/constants";
-import moment from "moment";
+import { JOIN_COLLECTION_ID } from "@/constants";
 
 export async function postRecentViewedProject(
   body: IPostRecentViewedProjectBody
@@ -19,33 +16,21 @@ export async function postRecentViewedProject(
   } catch (error) {
     throw new Error("Unable to get MongoDB client");
   }
-  // Database reference with creation if it does not already exist
-  const db = client.db(`codebox-live`);
-  console.log(`Using database:\t${db.databaseName}\n`);
-  const joinCollection = db.collection(JOIN_COLLECTION_ID);
   const join: Partial<IJoin> = {
     id1: userId,
     id1Type: JoinType.UserId,
     id2: body.projectId,
     id2Type: JoinType.ProjectId,
   };
-  console.log("postRecentViewedProject: attempting to post join", join);
-  let joins: IJoin[] = await joinCollection.find<IJoin>(join).toArray();
-  joins.sort((a, b) => {
-    const isAfter = moment(a.date).isBefore(b.date);
-    if (isAfter) {
-      return 1;
-    }
-    const isEqual = moment(a.date).isSame(b.date);
-    if (isEqual) {
-      return 0;
-    }
-    return -1;
-  });
-  const collection = db.collection(PROJECTS_COLLECTION_ID);
+  // Database reference with creation if it does not already exist
+  const db = client.db(`codebox-live`);
+  console.log(`Using database:\t${db.databaseName}\n`);
+  const collection = db.collection(JOIN_COLLECTION_ID);
   console.log(`Using collection:\t${collection.collectionName}\n`);
-  // Insert via upsert (create or replace) doc to collection directly
-  const insertResult = await collection.updateOne(
+  // select all that match containerId
+
+   // Insert via upsert (create or replace) doc to collection directly
+   const insertResult = await collection.updateOne(
     join,
     {
       $set: {
--- src/api/postTeamsPinnedProject.ts
+++ src/api/postTeamsPinnedProject.ts
@@ -1,12 +1,10 @@
 "use server";
-"use server"
 
 import { MongoClient } from "mongodb";
 import { getUserId } from ".";
 import { CodeboxMongoClient } from "./internal";
 import { IJoin, IPostTeamsPinnedProjectBody, JoinType } from "@/models";
-import { JOIN_COLLECTION_ID, PROJECTS_COLLECTION_ID } from "@/constants";
-import moment from "moment";
+import { JOIN_COLLECTION_ID } from "@/constants";
 
 export async function postTeamsPinnedProject(
   body: IPostTeamsPinnedProjectBody
@@ -19,29 +17,18 @@ export async function postTeamsPinnedProject(
     throw new Error("Unable to get MongoDB client");
   }
   // Database reference with creation if it does not already exist
-  const db = client.db(`codebox-live`);
-  console.log(`Using database:\t${db.databaseName}\n`);
-  const joinCollection = db.collection(JOIN_COLLECTION_ID);
   const join: Partial<IJoin> = {
     id1: body.threadId,
     id1Type: JoinType.TeamsThreadId,
     id2: body.projectId,
     id2Type: JoinType.ProjectId,
   };
-  let joins: IJoin[] = await joinCollection.find<IJoin>(join).toArray();
-  joins.sort((a, b) => {
-    const isAfter = moment(a.date).isBefore(b.date);
-    if (isAfter) {
-      return 1;
-    }
-    const isEqual = moment(a.date).isSame(b.date);
-    if (isEqual) {
-      return 0;
-    }
-    return -1;
-  });
-  const collection = db.collection(PROJECTS_COLLECTION_ID);
+  // Database reference with creation if it does not already exist
+  const db = client.db(`codebox-live`);
+  console.log(`Using database:\t${db.databaseName}\n`);
+  const collection = db.collection(JOIN_COLLECTION_ID);
   console.log(`Using collection:\t${collection.collectionName}\n`);
+
   // Insert via upsert (create or replace) doc to collection directly
   const insertResult = await collection.updateOne(
     join,
