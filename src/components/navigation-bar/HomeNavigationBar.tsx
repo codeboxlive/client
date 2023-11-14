ba5912897ae39f8c751156e39406039313c677bd
Ryan Bliss <smile@ryanbliss.me>
2023-11-13 21:03:27 -0800

    Log out button

--- src/components/navigation-bar/HomeNavigationBar.tsx
+++ src/components/navigation-bar/HomeNavigationBar.tsx
@@ -1,7 +1,9 @@
-import { Tab, TabList } from "@fluentui/react-components";
+import { Button, Tab, TabList } from "@fluentui/react-components";
 import { FC } from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import { NavigationBar } from "./NavigationBar";
+import Link from "next/link";
+import { inTeams } from "@/utils";
 
 export const HomeNavigationBar: FC = () => {
   const pathname = usePathname();
@@ -12,6 +14,13 @@ export const HomeNavigationBar: FC = () => {
         <TabList selectedValue={pathname}>
           <Tab value={"/projects"}>{"Projects"}</Tab>
         </TabList>
+      }
+      rightActions={
+        <Link href={`/api/auth/logout?returnTo=/?inTeams=${inTeams()}`}>
+          <Button size="small" appearance="subtle">
+            {"Log out"}
+          </Button>
+        </Link>
       }
     />
   );
