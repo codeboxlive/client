266b4e62fc8cd6bc2a8436edb0a3d94893607aaf
Ryan Bliss <smile@ryanbliss.me>
2023-11-17 21:03:24 -0800

    fixed msal import

--- src/api/msal-client.ts
+++ src/api/msal-client.ts
@@ -1,7 +1,7 @@
-import msal from "@azure/msal-node";
+import { ConfidentialClientApplication } from "@azure/msal-node";
 
 // Creating MSAL client
-export const msalClient = new msal.ConfidentialClientApplication({
+export const msalClient = new ConfidentialClientApplication({
   auth: {
     clientId: process.env.AAD_CLIENT_ID!,
     clientSecret: process.env.AAD_CLIENT_SECRET!,
